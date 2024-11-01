86127361471b7d1e0018ac666e64bea1346126f2
kisia0916 <fumiharuabe@gmail.com>
2024-11-02 01:12:58 +0900

    add count diff system

--- main.ts
+++ main.ts
@@ -88,41 +88,57 @@ export const get_hash = (data:string):string=>{
     return hash.digest("hex")
 }
 
-export const add_chain = (index:number,data:block_data_interface)=>{
-    // now_chain[index].push(data)
+export const add_chain = (chain_id:string,data:block_data_interface)=>{
+    const target_chain_index = now_chain.findIndex((i:chain_data_interface)=>i.chain_id === chain_id)
+    if (target_chain_index !== -1){
+        now_chain[target_chain_index].data.push(data)
+    }
 }
 export const add_new_chain_branch = (data:block_data_interface)=>{
     // now_chain.push([data])
 }
 
 
-export const count_all_diff = (count_chain:block_data_interface[]):number=>{
+export const count_all_diff = (chain_id:string,index:number):number=>{
         let all_diff:number = 0
-        count_chain.forEach((i:block_data_interface)=>{
-                all_diff+=i.nance_length**now_diff
-        })
+        const target_chain = now_chain.find((i:chain_data_interface)=>i.chain_id === chain_id)
+        if (target_chain){
+            if (index === -1){
+                index = target_chain.data.length-1
+            }
+            for (let i = 0;index>=i;i++){
+                all_diff+=target_chain.data[i].nance_length**now_diff
+            }
+            if (target_chain.root_block){
+                all_diff += count_all_diff(target_chain.root_block.chain_id,target_chain.root_block.block_num)
+            }
+        }
         return all_diff
 }
-const block_chain_proof = (chain_id:string,start_index:number):boolean=>{
+const block_chain_proof = (chain_id:string,last_hash:string,index:number):boolean=>{
     const target_chain:chain_data_interface|undefined = now_chain.find((i:chain_data_interface)=>i.chain_id === chain_id)
     if (target_chain){
-        console.log(target_chain)
+        const start_index = index >= 0?index:target_chain.data.length-1
+        //最後のブロックを検証
+        const {nance,...last_block_head} = target_chain.data[start_index]
+        const last_block_head_hash_value = get_hash(String(nance)+JSON.stringify(last_block_head))
+        if (last_hash){
+            if (last_block_head_hash_value !== last_hash){
+                return false
+            }
+        }else if (last_block_head_hash_value.slice(0,last_block_head.nance_length) !== nance_char.repeat(last_block_head.nance_length)){
+            return false
+        }
         //ブロックの後ろから整合性を確認
-        for (let i:number = target_chain.data.length-1;i>0;i--){
+        for (let i:number = start_index;i>0;i--){
             const {nance,...before_block_head} = target_chain.data[i-1]
             const before_block_hash_value = get_hash(String(nance)+JSON.stringify(before_block_head))
             if (before_block_hash_value !== target_chain.data[i].before_block_hash){
                 return false
             }
         }
-        //最後のブロックを検証
-        const {nance,...last_block_head} = target_chain.data[target_chain.data.length-1]
-        const last_block_head_hash_value = get_hash(String(nance)+JSON.stringify(last_block_head))
-        if (last_block_head_hash_value.slice(0,now_diff) !== nance_char.repeat(now_diff)){
-            return false
-        }
         if (target_chain.root_block){
-            return block_chain_proof(target_chain.root_block.chain_id,target_chain.root_block.block_num)
+            return block_chain_proof(target_chain.root_block.chain_id,target_chain.data[0].before_block_hash,target_chain.root_block.block_num)
         }
         return true
     }else{
@@ -131,15 +147,15 @@ const block_chain_proof = (chain_id:string,start_index:number):boolean=>{
 }
 //起動時にブロックチェーンの整合性確認
 
-// console.log(count_all_diff(now_chain))
-console.log(block_chain_proof("4cd3d5cd-8d33-457b-8313-2cc853d4f8ac",0))
 
-// server.listen(PORT,()=>{
-//     console.log(`This tsubame node is running!`)
-//     console.log(`Now using ip:${my_ip}`)
-//     if (now_chain.length > 1){
-//         console.log("now chain status:\x1b[31mCompetitioning\x1b[0m")
-//     }else{
-//         console.log("now chain status:\x1b[32mNormal\x1b[0m")
-//     }
-// })
+console.log(`chain proof:${block_chain_proof("4cd3d5cd-8d33-457b-8313-2cc853d4f8ac","",-1)}`)
+console.log(`chain all diff:${count_all_diff("9e7f0b28-5e1f-4f40-8f6f-c9b082886eca",-1)}`)
+server.listen(PORT,()=>{
+    console.log(`This tsubame node is running!`)
+    console.log(`Now using ip:${my_ip}`)
+    if (now_chain.length > 1){
+        console.log("Now chain status:\x1b[31mCompetitioning\x1b[0m")
+    }else{
+        console.log("Now chain status:\x1b[32mNormal\x1b[0m")
+    }
+})
